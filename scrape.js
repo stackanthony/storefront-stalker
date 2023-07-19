2bbcdb63c9b3b706f639eb1956e71f44afb59b30
gurubac <gbrbachchu@gmail.com>
2023-07-18 17:25:37 -0700

    add logs

--- scrape.js
+++ scrape.js
@@ -1,70 +1,69 @@
-const signale = require("signale");
+const fetch = require("node-fetch");
 const cheerio = require("cheerio");
+const signale = require("signale");
 
 const scrape = async (sellerID) => {
-    const queryURL = `https://www.amazon.com/s?i=merchant-items&me=${sellerID}`;
+	const queryURL = `https://www.amazon.com/s?i=merchant-items&me=${sellerID}`;
+	const headers = {
+		"User-Agent":
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+		Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+		"Accept-Language": "en-US,en;q=0.5",
+	};
 
-    try {
-        const res = await fetch(queryURL, {
-            headers: {
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-                Accept:
-                    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                "Accept-Language": "en-US,en;q=0.5",
-            },
-        });
+	try {
+		const fetchHTML = async (url) => {
+			const res = await fetch(url, { headers });
+			return await res.text();
+		};
 
-        const html = await res.text();
-        const $ = cheerio.load(html);
+		const getPageAsins = async (pageURL) => {
+			const pageHtml = await fetchHTML(pageURL);
+			const $page = cheerio.load(pageHtml);
+			return $page("div[data-asin]")
+				.map((i, ASIN) => $page(ASIN).attr("data-asin"))
+				.get()
+				.filter((asin) => asin && asin.trim() !== ""); // Exclude empty and whitespace-only ASINs
+		};
 
-        let resultsText = $("#search > span:nth-child(9) > div > h1 > div > div.sg-col-14-of-20.sg-col-18-of-24.sg-col.s-breadcrumb.sg-col-10-of-16.sg-col-6-of-12 > div > div > span").text();
-        signale.info(resultsText);
-        // console.log(resultCount);
+		const html = await fetchHTML(queryURL);
+		const $ = cheerio.load(html);
 
-        let sellerAsins = [];
+		const resultsText = $(
+			"#search > span:nth-child(9) > div > h1 > div > div.sg-col-14-of-20.sg-col-18-of-24.sg-col.s-breadcrumb.sg-col-10-of-16.sg-col-6-of-12 > div > div > span"
+		).text();
+		signale.info(resultsText);
 
-        if (resultsText.includes("-")) {
-            //contains more than 16 results, needs to paginate
-            let totalResults = resultsText.split(" ")[2];
-            let totalPageCount = Math.ceil(totalResults / 16);
-            signale.info(totalPageCount);
-            signale.info("Total Page Count: ", totalPageCount);
+		let sellerAsins = [];
 
-            for (let page = 1; page <= totalPageCount; page++) {
-                let pageURL = `${queryURL}&page=${page}`;
-                const pageRes = await fetch(pageURL, {
-                    headers: {
-                        "User-Agent":
-                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-                        Accept:
-                            "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                        "Accept-Language": "en-US,en;q=0.5",
-                    },
-                });
+		if (resultsText.includes("-")) {
+			// Contains more than 16 results, needs pagination
+			const totalResults = resultsText.split(" ")[2];
+			const totalPageCount = Math.ceil(totalResults / 16);
+			signale.info("Total Page Count: ", totalPageCount);
 
-                const pageHtml = await pageRes.text();
-                const $page = cheerio.load(pageHtml);
+			for (let page = 1; page <= totalPageCount; page++) {
+				signale.info("Scraping page: ", page);
+				const pageURL = `${queryURL}&page=${page}`;
+				const pageAsins = await getPageAsins(pageURL);
+				sellerAsins.push(...pageAsins);
+			}
+		} else {
+			// All results on one page
+			sellerAsins = await getPageAsins(queryURL);
+		}
+		// signale.success the scraped asins length and the num pages
+		signale.success("Scraped ASINs Count: ", sellerAsins.length);
+		signale.success(
+			"Scraped Page Count: ",
+			Math.ceil(sellerAsins.length / 16) + 1
+		);
 
-                $page("div[data-asin]")
-                    .map((i, ASIN) => sellerAsins.push($page(ASIN).attr("data-asin")))
-                    .get()
-                    .filter((asin) => asin);
-            }
-
-        } else {
-            //all results on one page
-            sellerAsins = $("div[data-asin]")
-                .map((i, ASIN) => $(ASIN).attr("data-asin"))
-                .get()
-                .filter((asin) => asin); // Filter out any empty asins
-        }
-
-        return sellerAsins;
-    } catch (error) {
-        signale.error("Error occurred while scraping:", error.message);
-        return [];
-    }
+		return sellerAsins;
+	} catch (error) {
+		signale.error("Error occurred while scraping:", error.message);
+		return [];
+	}
 };
 
 module.exports = scrape;
--- src/commands/seller/scrape.js
+++ src/commands/seller/scrape.js
@@ -1,15 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 const scrape = require("../../../scrape.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('scrape')
-		.setDescription('scrapes!'),
+	data: new SlashCommandBuilder().setName("scrape").setDescription("scrapes!"),
 	async execute(interaction) {
 		const sellerID = "A2QHNIQ1AC1T10";
-		const sellerAsins = await scrape(sellerID);
-		console.log(sellerAsins);
-		return interaction.reply("Done");
-		// return interaction.reply(`Seller ASINs: ${sellerAsins.join(", ")}`);
+
+		// Defer the reply before making the request
+		await interaction.deferReply();
+
+		try {
+			const sellerAsins = await scrape(sellerID);
+			// console.log(sellerAsins);
+
+			// Send the reply after the request is complete
+			await interaction.editReply(
+				sellerAsins ? sellerAsins.join(", ") : "No ASINs found"
+			);
+		} catch (error) {
+			console.error("Error occurred:", error);
+			await interaction.editReply("An error occurred while scraping.");
+		}
 	},
 };
