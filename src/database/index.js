aea104f82772c93b9841b196beb7787862c83c19
gurubac <gbrbachchu@gmail.com>
2023-07-19 14:43:19 -0700

    logging variable true false

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const signale = require("signale");
 const { Sequelize } = require("sequelize");
+
 try {
 	const sequelize = new Sequelize({
 		dialect: "postgres",
@@ -11,6 +12,7 @@ try {
 		username: process.env.DATABASE_USERNAME,
 		password: process.env.DATABASE_PASSWORD,
 		schema: process.env.DATABASE_SCHEMA,
+		logging: process.env.LOGGING === "true",
 	});
 
 	sequelize.query(
