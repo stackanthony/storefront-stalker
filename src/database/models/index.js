0e43d2c726c571e45c50d27af12dcbb601f8dea3
copbrick <anthonyjbenjamin@gmail.com>
2023-08-17 13:35:05 -0700

    removed unneccessary comments in database/models/index.js

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,8 +1,4 @@
 const User = require("./User");
 const Seller = require("./Seller");
 
-//Sets up One-To-Many Relationship
-// Seller.hasMany(User, { foreignKey: "usersTracking", sourceKey: "discordUserID" });
-// User.belongsTo(Seller);
-
 module.exports = { User, Seller };
